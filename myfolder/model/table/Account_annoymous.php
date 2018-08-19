ec2df8693e476735d3c7408c2201717b8413f589
youngxuji <youngxuji@qq.com>
2018-08-19 22:14:46 +0800

    增加了留言板和匿名账户的表和模型.太迟了我要睡觉了晚安明天还要上班嘤嘤嘤.

---
+++ myfolder/model/table/Account_annoymous.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 匿名账户表 model类
+ * Created by PhpStorm.
+ * User: 35906
+ * Date: 18.8.19
+ * Time: 21:58
+ */
+
+namespace model\table;
+
+
+use model\BaseTable;
+
+class Account_annoymous extends BaseTable
+{
+
+    //todo 实现匿名账户表
+    /**
+     *
+     */
+    function setRules()
+    {
+        // TODO: Implement setRules() method.
+    }
+}
---
+++ myfolder/model/table/Msg_board.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 留言板表 model类
+ * Created by PhpStorm.
+ * User: 35906
+ * Date: 18.8.19
+ * Time: 21:56
+ */
+
+namespace model\table;
+
+
+use model\BaseTable;
+
+class Msg_board extends BaseTable
+{
+    CONST MODE_INSERT = 0B1;//发表留言
+    //todo 留言板实现
+
+    /**
+     *
+     */
+    function setRules()
+    {
+        // TODO: Implement setRules() method.
+    }
+}
