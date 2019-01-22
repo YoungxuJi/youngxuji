d47ef0b030d326a7a4f391bbcfa5f40c0cffe222
youngxuji <youngxuji@qq.com>
2019-01-23 00:50:30 +0800

    增加注释

--- myfolder/model/SpecialDataType.php
+++ myfolder/model/SpecialDataType.php
@@ -8,7 +8,36 @@
 
 namespace model;
 
-
+/**
+ * Class SpecialDataType
+ * @package model
+ * <pre>
+ * $stdClass->type (string)
+ * $stdClass->option (mixed) 根据type确定数据类型//一般情况下是数据的值
+ * $stdClass->extra (mixed) 额外配置项,使用待定
+ *
+ * type:TYPE_FOREIGN_KEY 外键
+ * ->>option:string 外键值
+ * [->>extra:string 关联数据表名,如果key以 $tablename+_id 格式命名可以不用配置此option]
+ * For example:
+ *  $data['auth_id'] = (object)[
+ *      'type'=>TYPE_FOREIGN_KEY,
+ *      'option'=>1
+ *  ]
+ *
+ * type:TYPE_UNIQUE 唯一
+ * ->>option:string 值
+ * For example:
+ * $data['username'] = (object)[
+ *      'type'=>TYPE_UNIQUE,
+ *      'option'=>'myusername'
+ * ]
+ * 如果重复,则
+ * $this->error['error_field']=重复值的字段名称
+ *
+ * 调用函数,树
+ * </pre>
+ */
 class SpecialDataType
 {
     public $type;
